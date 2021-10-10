fb0ddd1847f9d2fef579a514eaa8e493f096197a
srushti0415 <srushtikulkarni15@gmail.com>
2021-10-10 10:44:32 +0530

    squareroot

---
+++ index1.js
@@ -0,0 +1,11 @@
+let b = 1;
+// let c = 1;
+function squareroot(a) {
+    while (b < a) {
+        if (b * b == a) {
+            console.log("Square root",b)
+        }
+    b++;
+    }
+}
+squareroot(64);
