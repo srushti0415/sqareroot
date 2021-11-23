929abbf0c015960ecaf3c5f001b3f2be3ee2e9bb
srushti0415 <srushtikulkarni15@gmail.com>
2021-11-23 22:07:15 +0530

    updated

---
+++ indx.js
@@ -0,0 +1,27 @@
+
+// function add(num1, num2) {
+//     console.log(num1 + num2)
+// }
+
+// add(2,6);
+
+// const Name = "holmes";l
+
+// function toUpper(text) {
+//     const upperCased = text.toUpperCase();
+//     console.log(upperCased);
+// }
+
+// toUpper(Name);
+
+
+const myName = sru;
+const age = 19;
+
+// console.log (`hello its me ${myName} n my ag is ${age}`);
+
+const add =  myName + age;
+console.log(add);
+
+
+// /home/srushti/tutorials /index2.js   path of this file
